1e3cfd2a1d535824b0f6e453a5d626e1b614ca56
a03theweirtvsweri-hue <a03theweirtvsweri@gmail.com>
2026-08-24 14:48:37 +0700

    Lab02 Part A

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -12,7 +12,31 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"Hp Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: 47%");
+
         }
     }
 }
