ba82d6add5f9eeef81a9654ccebf6012ac0f0534
a03theweirtvsweri-hue <a03theweirtvsweri@gmail.com>
2026-08-24 14:19:01 +0700

    Add header

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID : 1690700677
+ * Name       : Witawat Thawon
+ * Section    : 129A
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
 {
     internal class Program
     {
